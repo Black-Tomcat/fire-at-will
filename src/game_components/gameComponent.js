ce0c2e327383f622176ae892c3d36f498c3ec5d2
Black-Tomcat <tomcat.frantz@gmail.com>
2018-08-14 13:58:22 +1000

    Comments

--- src/game_components/gameComponent.js
+++ src/game_components/gameComponent.js
@@ -1,4 +1,11 @@
+// ./src/game_components/gameComponent.js
+
 export default class GameComponent {
+    /* The core of a game component is that each should have a reference to the
+     * parent object (spaceship, etc.), and that is where they gather all of
+     * their data from. They should also implement an update() method in which
+     * the GameCore calls, and updates the state relative to that component for
+     * the parent object. */
     constructor(parent) {
         this.parent = parent;
     }
