6b4170c0c8f13cfca169a50ca9a647c9887eb0c7
Black-Tomcat <tomcat.frantz@gmail.com>
2018-08-14 18:44:53 +1000

    Rough framework of the AI component

--- src/game_components/aiComponent.js
+++ src/game_components/aiComponent.js
@@ -1,7 +1,63 @@
 import GameComponent from "./gameComponent";
 
+
+const STANCES = {
+    AGGRESSIVE: "AGGRESSIVE",
+    DEFENSIVE: "DEFENSIVE",
+    RETREATING: "RETREATING"
+};
+
 export default class AIComponent extends GameComponent{
-    update(delta) {
+    constructor(parent) {
+        super(parent);
+
+        const {AGGRESSIVE, DEFENSIVE, RETREATING} = STANCES;
+        this.stance = null;
+
+        switch (parent.type.aiType) {
+            case AGGRESSIVE:
+                this.stance = AGGRESSIVE;
+                break;
+            case DEFENSIVE:
+                this.stance = DEFENSIVE;
+                break;
+            case RETREATING:
+                this.stance = RETREATING;
+                break;
+            default:
+                throw new Error("STANCE WAS NOT DEFINED FROM TYPE! STANCE: " + parent.type.aiType);
+        }
+
+        this.aiMap = {
+            AGGRESSIVE: this.aggressiveUpdate,
+            DEFENSIVE: this.defensiveUpdate,
+            RETREATING: this.retreatingUpdate
+        };
+    }
+
+    aggressiveUpdate = () => {
+        // Ramming only method c:
+
+    };
+
+    defensiveUpdate = () => {
+
+    };
+
+    retreatingUpdate = () => {
+
+    };
+
+    update(delta, fleets) {
+        // figure out stance. (Aggressive, Defensive, Passive?, Retreating)
+        // Now, dependant on stance, update state of AI.
+        this.updateStance();
+
+        // Run that states update method
+        this.aiMap[this.stance]();
+    }
+
+    updateStance = () => {
 
     }
 }
